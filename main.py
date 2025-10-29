e1de6e8850617c60a44e8ac8c478be188f22e9d3
Stoyan Atanasov <stoiancho@gmail.com>
2025-10-29 21:43:31 +0000

    Refactor main.py to implement SQLAlchemy ORM with User model and update requirements for PostgreSQL support

--- main.py
+++ main.py
@@ -1,14 +1,63 @@
-from sqlalchemy import create_engine, text
+from uuid import uuid4
+from sqlalchemy import UUID, String, create_engine, text
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase, mapped_column
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine("postgresql+asyncpg://postgres:postgres@localhost/postgres", echo=True)
+engine = create_engine(
+    "postgresql://postgres:postgres@localhost/postgres", echo=True)
 
 
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT 1"))
-    for row in result:
-        print(row)
+# with engine.connect() as connection:
+#     result = connection.execute(text("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name VARCHAR(50));"))
+#     result = connection.execute(text("INSERT INTO test_table (name) VALUES (:name)"), [{"name": "Alice"}, {"name": "Bob"}])
+#     connection.commit()
+#     # for row in result:
+#     #     print(row)
 
+# with engine.begin() as connection:
+#     result = connection.execute(text("INSERT INTO test_table (name) VALUES (:name)"), [{"name": "Charlie"}, {"name": "Diana"}])
+
+
+# with engine.connect() as connection:
+#     result = connection.execute(text("SELECT * FROM test_table;"))
+#     for row in result:
+#         print(f"id: {row.id}, name: {row.name}")
+
+# with Session(engine) as session:
+#     result = session.execute(text("SELECT * FROM test_table;"))
+#     for row in result:
+#         print(f"id: {row.id}, name: {row.name}")
+
+Session = sessionmaker(bind=engine)
+
+# with Session() as session, session.begin():
+#     result = session.execute(text("INSERT INTO test_table (name) VALUES (:name)"), [{"name": "Alice"}, {"name": "Bob"}])
+
+
+# with Session() as session:
+#     result = session.execute(text("SELECT * FROM test_table;"))
+#     for row in result:
+#         print(f"id: {row.id}, name: {row.name}")
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    id = mapped_column(UUID, primary_key=True, server_default=text("gen_random_uuid()"))
+    name = mapped_column(String)
+
+
+Base.metadata.create_all(engine)
+
+# with Session() as session:
+#     session.add(User(id=uuid4(), name="Alice"))
+#     session.commit()
+
+with Session() as session:
+    users = session.query(User).all()
+    for user in users:
+        print(f"id: {user.id}, name: {user.name}")
