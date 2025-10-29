91623c6c71e76bbda139fc89e6c86ee3e2f27b17
Stoyan Atanasov <stoiancho@gmail.com>
2025-10-29 20:59:42 +0000

    Update Dockerfile, main.py, and requirements for PostgreSQL integration and cleanup

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine("postgresql+asyncpg://user:password@localhost/dbname")
+engine = create_engine("postgresql+asyncpg://postgres:postgres@localhost/postgres", echo=True)
+
+
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT 1"))
+    for row in result:
+        print(row)
 
